ff9361538c3ead8a9192b7a9d7a26a1614059143
David Palmerin <david.palmerin@Davids-MacBook-Pro.local>
2020-03-06 15:15:09 -0600

    refactored

--- Pingy/src/routes/conversation.js
+++ Pingy/src/routes/conversation.js
@@ -1,48 +1,57 @@
 import {Router} from 'express';
+import mongoose, { models } from 'mongoose';
 
 const router = new Router();
 
-const hasConversation = async (user, conversationId) => {
-    var autenticated = false;
-    await user.conversations.forEach(id => {
+const hasConversation = (user, conversationId) => {
+    var authenticated = false;
+    user.conversations.forEach(id => {
         if (id.toString() == conversationId)
-            autenticated = true;
+            authenticated = true;
     });
 
-    return autenticated;
+    return authenticated;
 }
 
 router.get('/:conversationId', async (req, res) => {
-    const user = await req.context.models.User.findById(req.context.me)
+    await req.context.models.User.findById(req.context.me.id)
+    .then(user => {
+        const authenticated = hasConversation(user, req.params.conversationId);
+        if (authenticated) {
+            req.context.models.Conversation.findById(req.params.conversationId)
+            .then(async conversation => {
+                var thread = await Promise.all(conversation.messages.map(async id => {
+                    const message = await req.context.models.Message.findById(id);
+                    console.log(message)
+                    return message.text;
+                }));
+                return res.send(thread);
+                //return res.send(conversation);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    })
     .catch(err => {
         console.log(err);
     });
-    const autenticated = await hasConversation(user, req.params.conversationId);
-    if (autenticated) {
-        console.log('autenticated');
-        const conversation = await req.context.models.Conversation.findById(req.params.conversationId)
-        .catch(err => {
-            console.log(err);
-        });
-        var thread = await Promise.all(conversation.messages.map(async id => {
-            const message = await req.context.models.Message.findById(id);
-            return message.text;
-        }));
-        return res.send(thread);
-    }
-    return res.send([]);
 });
 
 router.post('/:conversationId', async (req, res) => {
-    console.log('Post');
-    req.context.models.User.findById(req.context.me._id)
+    await req.context.models.User.findById(req.context.me.id)
     .then(async user => {
-        const autenticated = await hasConversation(user, req.params.conversationId);
-        if (autenticated) {
-            console.log('autenticated');
+        const authenticated = hasConversation(user, req.params.conversationId);
+        if (authenticated) {
+            console.log('authenticated');
+            const message = new models.Message({
+                user: req.context.me.id,
+                text: req.body.text
+            });
+            message.save();
             req.context.models.Conversation.updateOne(
                 {"_id": req.params.conversationId}, 
-                {"$push" : {"messages" : req.body.text}},
+                {"$push" : {"messages": message}},
                 function (err) {
                     if (err) 
                         console.log(err); 
